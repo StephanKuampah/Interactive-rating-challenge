2a8edfa52b2fe682e9ad4772aa1dbb34401cb9ba
StephKuampah <Kuampahstephan>
2023-10-31 16:39:05 +0000

    javascript added

---
+++ Interactive_rating_challenge.js
@@ -0,0 +1,20 @@
+const rate_button1 = document.getElementById("b1");
+const rate_button2 = document.getElementById("b2");
+const rate_button3 = document.getElementById("b3");
+const rate_button4 = document.getElementById("b4");
+const rate_button5 = document.getElementById("b5");
+const submit_button = document.getElementById("submit_button");
+
+
+
+
+function select_button(e){
+    console.log(e)
+}
+
+rate_button1.addEventListener("click",select_button);
+rate_button2.addEventListener("click",select_button);
+rate_button3.addEventListener("click",select_button);
+rate_button4.addEventListener("click",select_button);
+rate_button5.addEventListener("click",select_button);
+submit_button.addEventListener("click",select_button);
